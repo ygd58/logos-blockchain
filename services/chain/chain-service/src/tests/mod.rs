63c7ac95cff990597b945ff375ccf11c4817c136
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2026-03-08 15:40:13 +0000

    Set balances on channels.

--- services/chain/chain-service/src/tests/mod.rs
+++ services/chain/chain-service/src/tests/mod.rs
@@ -63,9 +63,10 @@ fn cryptarchia_switch_to_online() {
         assert!(reorged_blocks.is_empty());
 
         block_ids.push(block.header().id());
+
         slot = block.header().slot() + 1;
     }
-    {}
+
     // Now, the chain is [G, B1, B2, B3].
     // We now switch to Online and check that LIB advances to B2.
     let (cryptarchia, pruned_blocks) = cryptarchia.online();
