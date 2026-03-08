a6e335341d57f61d3bb6c473a12290fb0109faf8
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2026-03-08 15:15:57 +0000

    Implement DecodeOp: entity, decoding, encoding and gas.

--- services/chain/chain-service/src/tests/mod.rs
+++ services/chain/chain-service/src/tests/mod.rs
@@ -66,7 +66,7 @@ fn cryptarchia_switch_to_online() {
         cryptarchia = new_cryptarchia;
         slot = block.header().slot() + 1;
     }
-
+    {}
     // Now, the chain is [G, B1, B2, B3].
     // We now switch to Online and check that LIB advances to B2.
     let (cryptarchia, pruned_blocks) = cryptarchia.online();
