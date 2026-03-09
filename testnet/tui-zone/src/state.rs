e01d478547fd198f53fcf0089de451dfaed9e460
Youngjoon Lee <5462944+youngjoon-lee@users.noreply.github.com>
2026-03-09 14:50:40 +0000

    define StateTransition and inscribe it as JSON

---
+++ testnet/tui-zone/src/state.rs
@@ -0,0 +1,75 @@
+use std::{collections::HashMap, ops::AddAssign as _};
+
+use lb_core::{
+    mantle::Value,
+    utils::{display_hex_bytes_newtype, serde_bytes_newtype},
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum StateTransition {
+    Mint { amount: Value, address: Address },
+}
+
+#[derive(Debug, Clone)]
+pub struct State {
+    accounts: HashMap<Address, Value>,
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {
+            accounts: HashMap::new(),
+        }
+    }
+
+    pub fn apply(&mut self, transition: &StateTransition) {
+        match transition {
+            StateTransition::Mint { amount, address } => {
+                self.mint(*address, *amount);
+            }
+        }
+    }
+
+    fn mint(&mut self, address: Address, amount: Value) -> Value {
+        *self
+            .accounts
+            .entry(address)
+            .and_modify(|balance| balance.add_assign(amount))
+            .or_insert(amount)
+    }
+
+    pub fn balance(&self, address: &Address) -> Option<Value> {
+        self.accounts.get(address).copied()
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub struct Address([u8; 32]);
+serde_bytes_newtype!(Address, 32);
+display_hex_bytes_newtype!(Address);
+
+impl Address {
+    pub const fn as_bytes(&self) -> &[u8; 32] {
+        &self.0
+    }
+}
+
+impl From<[u8; 32]> for Address {
+    fn from(addr: [u8; 32]) -> Self {
+        Self(addr)
+    }
+}
+
+impl TryFrom<&[u8]> for Address {
+    type Error = ();
+
+    fn try_from(addr: &[u8]) -> Result<Self, Self::Error> {
+        if addr.len() != 32 {
+            return Err(());
+        }
+        let mut array = [0u8; 32];
+        array.copy_from_slice(addr);
+        Ok(Self(array))
+    }
+}
